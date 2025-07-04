eea78b0a57f34099bc82cb15f47922ba60caee0f
TheDoEverythingLady <mag.sothebys2@gmail.com>
2025-07-04 11:52:20 +0200

    Update seed.sql

--- backend/seed.sql
+++ backend/seed.sql
@@ -1,1 +1,13 @@
--- Seed data for DB
+-- seed.sql
+INSERT INTO facilities (name) VALUES ('Facility A'), ('Facility B'), ('Facility C');
+
+INSERT INTO items (name, group, category, default_unit) VALUES
+('Beef Mince', 'Frozen', 'Beef', 'kg'),
+('Tomato Paste', 'Dry Shelf Products', 'Canned', 'g'),
+('Onions', 'Fresh', 'Vegetables', 'kg'),
+('Sunflower Oil', 'Dry Shelf Products', 'Oils', 'ml'),
+('Surgical Gloves', 'Non-Food', 'Medical Supplies', 'box');
+
+INSERT INTO users (email, password, role) VALUES
+('admin@ops.com', '$2a$10$3PaIbzCrkPRTRI3JIQkl0Od6mnn4TPMRIMrlod3xRmGIDRZQEGGvq', 'admin'), -- Password: admin123
+('stock@ops.com', '$2a$10$3PaIbzCrkPRTRI3JIQkl0Od6mnn4TPMRIMrlod3xRmGIDRZQEGGvq', 'stock_clerk'); -- Password: admin123
