81669ce4df5594c0378eb06833c28c26e15da129
TheDoEverythingLady <mag.sothebys2@gmail.com>
2025-07-04 12:52:54 +0200

    Update App.jsx

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -1,1 +1,20 @@
-// App entry point
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Dashboard from './pages/Dashboard';
+import Recipes from './pages/Recipes';
+import StockControl from './pages/StockControl';
+import Receipts from './pages/Receipts';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/recipes" element={<Recipes />} />
+        <Route path="/stock" element={<StockControl />} />
+        <Route path="/receipts" element={<Receipts />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
