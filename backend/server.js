9b2e79e2b64bff8047406d91e91014a044f87e08
TheDoEverythingLady <mag.sothebys2@gmail.com>
2025-07-04 13:21:07 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const authRoutes = require('./routes/authRoutes');
+const authRoutes = require('./routes/auth');
 app.use('/api/auth', authRoutes);
 
 app.get('/', (req, res) => res.send('OpsManager Backend Running'));
